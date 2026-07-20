0b4fc89f58a886367f103ab5104efb3c6c959896
HERPRISTANTI <nurfaizah.herpristanti@binus.ac.id>
2026-07-21 01:05:11 +0700

    chore: initialize Nuxt project structure

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Susi Air Crew Dashboard</h1>
+
+    <p>Nuxt 3 is running successfully 🚀</p>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
